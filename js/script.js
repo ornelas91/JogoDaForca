cf59cddce26dce6df2f2b99b815ffe52b08d043d
ornelas91 <anderson_ornelas91@hotmail.com>
2022-03-05 23:18:03 -0300

    Finalizando projeto

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+let tentativas = 6;
+let listaDinamica = [];
 let palavraSecretaCategoria;
 let palavraSecretaSorteada;
 const palavras = [
@@ -207,6 +209,7 @@ const palavras = [
     }
 ];
 
+criarPalavraSecreta();
 function criarPalavraSecreta() {
     const indexPalavra = parseInt(Math.random() * palavras.length)
 
@@ -216,4 +219,109 @@ function criarPalavraSecreta() {
     console.log(palavraSecretaCategoria)
 }
 
-criarPalavraSecreta();
+montarPalavraNaTela();
+function montarPalavraNaTela(){
+    const categoria = document.getElementById("categoria");
+    categoria.innerHTML = palavraSecretaCategoria;
+
+    const palavraTela = document.getElementById("palavra-secreta");
+    palavraTela.innerHTML = "";
+
+    for(i = 0; i < palavraSecretaSorteada.length; i++) {
+        if (listaDinamica[i] == undefined){
+            listaDinamica[i] = "&nbsp;"
+            palavraTela.innerHTML = palavraTela.innerHTML + "<div class='letras'>"+listaDinamica[i]+"</div>"
+        }
+        else {
+            palavraTela.innerHTML = palavraTela.innerHTML + "<div class='letras'>"+listaDinamica[i]+"</div>"
+        }
+    }
+}
+
+function verificaLetraEscolhida(letra){
+    document.getElementById("tecla-" + letra).disabled = true;
+    if (tentativas > 0){
+        mudarStyleLetra("tecla-" + letra);
+        comparalistas(letra);
+        montarPalavraNaTela();
+    }
+}
+
+function mudarStyleLetra(tecla){
+    document.getElementById(tecla).style.background = "#C71585";
+    document.getElementById(tecla).style.color = "#ffffff";
+}
+
+function comparalistas(letra){
+    const pos = palavraSecretaSorteada.indexOf(letra)
+    if(pos < 0){
+        tentativas--
+        carregaImagemForca();
+
+        if(tentativas == 0){
+            abreModal("OPS!", "Não foi dessa vez... A palavra secreta era <br>" + palavraSecretaSorteada);
+        }
+    }
+    else {
+        for(i = 0; i < palavraSecretaSorteada.length; i++){
+            if(palavraSecretaSorteada[i] == letra){
+                listaDinamica[i] = letra;
+            }
+        }
+    }
+
+    let vitoria = true;
+    for(i = 0; i < palavraSecretaSorteada.length; i++){
+        if(palavraSecretaSorteada[i] != listaDinamica[i]){
+            vitoria = false;
+        }
+    }
+    if(vitoria == true){
+        abreModal("Parabéns!", "Você venceu... <br>" + palavraSecretaSorteada);
+        tentativas = 0;
+    }
+}
+
+function carregaImagemForca(){
+    switch(tentativas) {
+        case 5:
+            document.getElementById("imagem").style.background = "url('../img/forca01.png')";
+            break;
+        case 4:
+            document.getElementById("imagem").style.background = "url('../img/forca02.png')";
+            break;
+        case 3:
+            document.getElementById("imagem").style.background = "url('../img/forca03.png')";
+            break;
+        case 2:
+            document.getElementById("imagem").style.background = "url('../img/forca04.png')";
+            break;
+        case 1:
+            document.getElementById("imagem").style.background = "url('../img/forca05.png')";
+            break;
+        case 0:
+            document.getElementById("imagem").style.background = "url('../img/forca06.png')";
+            break;
+        default:
+            document.getElementById("imagem").style.background = "url('../img/forca.png')";
+            break;
+    }
+}
+
+function abreModal( titulo, mensagem){
+    let modalTitulo = document.getElementById("exampleModalLabel");
+    modalTitulo.innerText = titulo;
+
+    let modalBody = document.getElementById("modalBody");
+    modalBody.innerHTML = mensagem;
+
+
+    $("#myModal").modal({
+        show: true
+    });
+}
+
+let btnReiniciar = document.querySelector("#btnReiniciar")
+btnReiniciar.addEventListener("click", function(){
+    location.reload();
+});
